2c30b67b36d9bf28586e3b54aacbff4449aebe4a
xioperez01 <angie.xioperez@gmail.com>
2020-12-08 01:08:30 +0000

    Initial code task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # create a file in /tmp.
 
-file {'/tmp/holberton':
-ensure => file,
-path => '/tmp/holbeton',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
-content => 'I love Puppet'
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
