329a3617327cbbacef9426a1f3e6846d79f29300
xioperez01 <angie.xioperez@gmail.com>
2020-12-22 15:27:41 +0000

    Advanced task

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,28 @@
 # creating a custom HTTP header response with Puppet.
 
 exec { 'exec0':
-  cmd => 'sudo sudo apt-get update -y',
+  command => 'sudo sudo apt-get update -y',
   path    => ['/usr/bin', '/bin'],
-  return => [0,1]
+  returns => [0,1]
 }
 
 exec { 'exec1':
   require => Exec['exec0'],
-  cmd => 'sudo apt-get install nginx -y',
+  command => 'sudo apt-get install nginx -y',
   path    => ['/usr/bin', '/bin'],
-  return => [0,1]
+  returns => [0,1]
 }
 
 exec { 'exec2':
   require => Exec['exec1'],
-  cmd => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-enabled/default',
+  command => 'sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \$hostname;/" /etc/nginx/sites-enabled/default',
   path    => ['/usr/bin', '/bin'],
-  return => [0,1]
+  returns => [0,1]
 }
 
 exec { 'exec3':
   require => Exec['exec2'],
-  cmd => 'sudo service nginx start',
+  command => 'sudo service nginx start',
   path    => ['/usr/bin', '/bin', '/usr/sbin'],
-  return => [0,1]
+  returns => [0,1]
 }
