c250448f7278163dc82e8b5f7f8efb4c15d663db
xioperez01 <angie.xioperez@gmail.com>
2021-02-18 17:35:09 +0000

    path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
 # Fix a 500 error in Wordpress settings file
+
 exec { 'fix config':
   command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-  path    => '/usr/bin/env',
+  path    => '/usr/local/sbin',
 }
